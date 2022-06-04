a9961a74c053b0254c54fd941dbabfefad84b9f2
ljubomirm2002 <matovicljubomir2002@gmai.com>
2022-06-04 16:45:13 +0200

    Added Role model

---
+++ main/src/main/java/com/ldg/main/Models/Role.java
@@ -0,0 +1,30 @@
+package com.ldg.main.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    public Role(){
+    }
+    public Role(Integer id) {
+        this.id = id;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+    
+    public Integer getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
