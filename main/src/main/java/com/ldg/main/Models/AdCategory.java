1eb6f2df1b7e82b9dd78adb7934d785b67a270c6
ljubomirm2002 <matovicljubomir2002@gmai.com>
2022-06-04 18:09:30 +0200

    AdCategory Model added

---
+++ main/src/main/java/com/ldg/main/Models/AdCategory.java
@@ -0,0 +1,36 @@
+package com.ldg.main.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "advertisment_categories")
+public class AdCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    public AdCategory() {
+    }
+
+    public AdCategory(Integer id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+}
