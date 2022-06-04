f9d8df6ed976d5a5aaea77a8ca1fc1b1a55a93f4
ljubomirm2002 <matovicljubomir2002@gmai.com>
2022-06-04 17:53:31 +0200

    Repository names changed

--- main/src/main/java/com/ldg/main/Repository/RoleRepository.java
+++ main/src/main/java/com/ldg/main/Repository/RoleRepository.java
@@ -3,8 +3,8 @@ package com.ldg.main.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ldg.main.Models.User;
+import com.ldg.main.Models.Role;
 
 @Repository
-public interface UserRepo extends JpaRepository<User,Integer> {
+public interface RoleRepository extends JpaRepository<Role,Integer> {
 }
--- main/src/main/java/com/ldg/main/Repository/UserRepository.java
+++ main/src/main/java/com/ldg/main/Repository/UserRepository.java
@@ -3,8 +3,8 @@ package com.ldg.main.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ldg.main.Models.Role;
+import com.ldg.main.Models.User;
 
 @Repository
-public interface RoleRepo extends JpaRepository<Role,Integer> {
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
