33cfdcc3bcf9708103211705d163695bde6870ba
ljubomirm2002 <matovicljubomir2002@gmai.com>
2022-06-05 16:53:52 +0200

    Database updated

--- main/src/main/resources/database/skuci_se.sql
+++ main/src/main/resources/database/skuci_se.sql
@@ -180,7 +180,7 @@ CREATE TABLE `users` (
   `image_path` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `role_id` int(11) NOT NULL,
   `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
